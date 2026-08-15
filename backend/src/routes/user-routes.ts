66142c4e0972f99bc03148c2d83a6f3d19a80db6
RenanTheFato <renan.santana007@hotmail.com>
2026-08-15 18:04:24 -0300

    add auth route to users

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,13 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "../controllers/user/create-user-controller.js";
+import { AuthUserController } from "../controllers/user/auth-user-controller.js";
 
 export const userRoutes = Router()
 
-userRoutes.post("/create-user", async (req: Request, res: Response) => {
+userRoutes.post("/create", async (req: Request, res: Response) => {
   return new CreateUserController().handle(req, res)
+})
+
+userRoutes.post("/auth", async (req: Request, res: Response) => {
+  return new AuthUserController().handle(req, res)
 })
