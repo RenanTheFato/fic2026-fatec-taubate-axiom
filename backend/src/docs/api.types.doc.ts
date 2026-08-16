d76359b9d4a56c99644b4905c29904651b68d409
RenanTheFato <renan.santana007@hotmail.com>
2026-08-16 19:39:37 -0300

    add api "translator" types for an zod doc schema

---
+++ backend/src/docs/api.types.doc.ts
@@ -0,0 +1,11 @@
+import type { ZodType } from "zod/v4";
+
+export interface ApiDoc<TBody extends ZodType | undefined = ZodType | undefined, 
+  TResponses extends Record<number, ZodType> = Record<number, ZodType>> {
+  tags: string[],
+  summary: string,
+  description: string,
+  security?: { bearerAuth: string[] }[],
+  body?: TBody,
+  response: TResponses,
+}
