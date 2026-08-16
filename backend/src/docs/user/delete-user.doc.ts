9e1251b6aab14ef81e44d6ee53cdb6cd89c7b610
RenanTheFato <renan.santana007@hotmail.com>
2026-08-16 20:15:58 -0300

    create doc for user deletion

---
+++ backend/src/docs/user/delete-user.doc.ts
@@ -0,0 +1,23 @@
+import { z } from "zod/v4";
+
+export const deleteUserDoc = {
+  tags: ["user", "delete"],
+  summary: "Delete a user by ID",
+  description: "Removes a registered user from the system using their unique identifier (ID). Requires authentication.",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  response: {
+    204: z.object({}).describe("Deleted successful, no content on response."),
+    
+    400: z.object({
+      error: z.string(),
+    }).describe("Invalid or missing user ID, or an error occurred while attempting to delete the user."),
+
+    404: z.object({
+      error: z.string(),
+    }).describe("User not registered, impossible to delete."),
+  },
+}
