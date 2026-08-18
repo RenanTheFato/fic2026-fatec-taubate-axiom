8fbebb99516407a54e009abeeb1082372e2c6603
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 18:23:29 -0300

    create cancel campaign doc

---
+++ backend/src/docs/campaign/cancel-campaign.doc.ts
@@ -0,0 +1,37 @@
+import { z } from "zod/v4";
+
+export const cancelCampaignDoc = {
+  tags: ["campaign"],
+  summary: "Cancel an Campaign",
+  description: "Turn an active or draft campaign into an cancelled",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  response: {
+    200: z.object({
+      message: z.string()
+    }).describe("Status changed sucessful"),
+
+    400: z.object({
+      error: z.string(),
+    }).describe("Bad Request — Validation failure or business rule violation."),
+
+    401: z.object({
+      error: z.string(),
+    }).describe("Unauthorized. Missing, invalid or expired JWT token."),
+
+    403: z.object({
+      error: z.string()
+    }).describe("Forbidden — The authenticated user's role is not allowed to publish campaigns."),
+
+    404: z.object({
+      error: z.string(),
+    }).describe("Campaign not found."),
+
+    500: z.object({
+      error: z.string(),
+    }).describe("Unexpected server error."),
+  },
+}
