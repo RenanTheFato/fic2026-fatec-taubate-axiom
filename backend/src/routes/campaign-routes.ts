b6f09bcd1d938141a54aaeb8eceb898970b8f907
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 18:03:40 -0300

    insert list all campaigns into routes

--- backend/src/routes/campaign-routes.ts
+++ backend/src/routes/campaign-routes.ts
@@ -5,6 +5,7 @@ import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { ListCampaignsController } from "../controllers/campaign/list-campaigns-controller.js";
 import { GetCampaignBySlugController } from "../controllers/campaign/get-campaign-by-slug-controller.js";
 import { PublishCampaignController } from "../controllers/campaign/publish-campaign-controller.js";
+import { ListAllCampaignsController } from "../controllers/campaign/list-all-campaigns-controller.js";
 
 export const campaignRoutes = Router()
 
@@ -14,6 +15,10 @@ campaignRoutes.post("/create", AuthMiddleware, RoleMiddleware("admin", "staff"),
 
 campaignRoutes.get("/list", async (req: Request, res: Response) => {
   return new ListCampaignsController().handle(req, res)
+})
+
+campaignRoutes.get("/list-all", AuthMiddleware, RoleMiddleware("admin", "staff"), async (req: Request, res: Response) => {
+  return new ListAllCampaignsController().handle(req, res)
 })
 
 campaignRoutes.get("/:slug", async (req: Request, res: Response) => {
