063c0ca3ed85970170a4f9f7db59a705a52e78f1
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 17:41:34 -0300

    create publish campaign doc

---
+++ backend/src/docs/campaign/publish-campaign.doc.ts
@@ -0,0 +1,33 @@
+import { z } from "zod/v4";
+
+export const deleteUserDoc = {
+  tags: ["campaign"],
+  summary: "Publish an Campaign",
+  description: "Turn an draft campaign into an active and visible publicly",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  response: {
+    200: z.object({
+      message: z.string()
+    }).describe("Published sucessful"),
+
+    400: z.object({
+      error: z.string(),
+    }).describe("Bad Request — Validation failure or business rule violation."),
+
+    401: z.object({
+      error: z.string(),
+    }).describe("Unauthorized. Missing, invalid or expired JWT token."),
+
+    403: z.object({
+      error: z.string()
+    }).describe("Forbidden — The authenticated user's role is not allowed to publish campaigns."),
+
+    404: z.object({
+      error: z.string(),
+    }).describe("Campaign not found."),
+  },
+}
