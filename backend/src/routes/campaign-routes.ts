8dfbebeaa5520f09eae92271e1f387f383a68ad3
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 15:03:14 -0300

    add list campaigns into routes

--- backend/src/routes/campaign-routes.ts
+++ backend/src/routes/campaign-routes.ts
@@ -2,9 +2,14 @@ import { Router, Request, Response } from "express";
 import { CreateCampaignController } from "../controllers/campaign/create-campaign-controller.js";
 import { AuthMiddleware } from "../middlewares/auth-middleware.js";
 import { RoleMiddleware } from "../middlewares/role-middleware.js";
+import { ListCampaignsController } from "../controllers/campaign/list-campaigns-controller.js";
 
 export const campaignRoutes = Router()
 
 campaignRoutes.post("/create", AuthMiddleware, RoleMiddleware("admin", "staff"), async (req: Request, res: Response) => {
   return new CreateCampaignController().handle(req, res)
+})
+
+campaignRoutes.get("/list", async(req: Request, res: Response) => {
+  return new ListCampaignsController().handle(req, res)
 })
