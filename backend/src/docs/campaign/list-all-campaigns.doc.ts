2e08b454f26b8b6a6d15929ad7c3fc5803d45e86
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 18:04:51 -0300

    create list all campaigns doc

---
+++ backend/src/docs/campaign/list-all-campaigns.doc.ts
@@ -0,0 +1,49 @@
+import { z } from "zod/v4";
+
+const validationErrorSchema = z.object({
+  message: z.string(),
+  errors: z.array(z.object({
+    code: z.string(),
+    message: z.string(),
+    path: z.string(),
+  })),
+}).describe("Input validation failed due to incorrect or missing data.")
+
+export const listCampaignsDoc = {
+  tags: ["campaign"],
+  summary: "View all campaigns",
+  description: "Fetches the campaigns",
+  response: {
+    200: z.object({
+      message: z.string()
+        .describe("Success message."),
+      campaigns: z.array(z.object({
+        id: z.string(),
+        title: z.string(),
+        slug: z.string(),
+        description: z.string().nullable(),
+        goal_amount: z.string(),
+        raised_amount: z.string(),
+        starts_at: z.iso.datetime(),
+        ends_at: z.iso.datetime().nullable(),
+        status: z.string(),
+        created_at: z.iso.datetime(),
+        updated_at: z.iso.datetime(),
+      }))
+    }).describe("Campaigns successfully fetched."),
+
+    400: validationErrorSchema.describe("Bad Request — Validation failure or business rule violation."),
+
+    401: z.object({
+      error: z.string(),
+    }).describe("Unauthorized. Missing, invalid or expired JWT token."),
+
+    403: z.object({
+      error: z.string()
+    }).describe("Forbidden — The authenticated user's role is not allowed to publish campaigns."),
+
+    500: z.object({
+      error: z.string(),
+    }).describe("Unexpected server error."),
+  }
+}
