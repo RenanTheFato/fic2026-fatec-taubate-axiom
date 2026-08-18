8a359e681e90f5b52b0260bd7a12de2ae0925610
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 17:37:22 -0300

    insert publish campaign into routes

--- backend/src/routes/campaign-routes.ts
+++ backend/src/routes/campaign-routes.ts
@@ -4,6 +4,7 @@ import { AuthMiddleware } from "../middlewares/auth-middleware.js";
 import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { ListCampaignsController } from "../controllers/campaign/list-campaigns-controller.js";
 import { GetCampaignBySlugController } from "../controllers/campaign/get-campaign-by-slug-controller.js";
+import { PublishCampaignController } from "../controllers/campaign/publish-campaign-controller.js";
 
 export const campaignRoutes = Router()
 
@@ -11,10 +12,14 @@ campaignRoutes.post("/create", AuthMiddleware, RoleMiddleware("admin", "staff"),
   return new CreateCampaignController().handle(req, res)
 })
 
-campaignRoutes.get("/list", async(req: Request, res: Response) => {
+campaignRoutes.get("/list", async (req: Request, res: Response) => {
   return new ListCampaignsController().handle(req, res)
 })
 
-campaignRoutes.get("/:slug", async(req: Request, res: Response) => {
+campaignRoutes.get("/:slug", async (req: Request, res: Response) => {
   return new GetCampaignBySlugController().handle(req, res)
+})
+
+campaignRoutes.patch("/publish/:campaign_id", AuthMiddleware, RoleMiddleware("admin", "staff"), async (req: Request, res: Response) => {
+  return new PublishCampaignController().handle(req, res)
 })
