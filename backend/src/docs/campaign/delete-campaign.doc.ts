3f94deb8c04e1f732e3bb52b93b3acc2245e47df
RenanTheFato <renan.santana007@hotmail.com>
2026-08-18 18:36:56 -0300

    create delete campaign doc

---
+++ backend/src/docs/campaign/delete-campaign.doc.ts
@@ -0,0 +1,35 @@
+import { z } from "zod/v4";
+
+export const deleteCampaignDoc = {
+  tags: ["campaign"],
+  summary: "Delete a campaign",
+  description: "Removes a registered campaign from the system where status equals draft. Only by admins",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  response: {
+    204: z.object({}).describe("Deleted successful, no content on response."),
+    
+    400: z.object({
+      error: z.string(),
+    }).describe("Bad Request — Validation failure or business rule violation."),
+
+    401: z.object({
+      error: z.string(),
+    }).describe("Unauthorized. Missing, invalid or expired JWT token."),
+
+    403: z.object({
+      error: z.string()
+    }).describe("Forbidden — The authenticated user's role is not allowed to publish campaigns."),
+
+    404: z.object({
+      error: z.string(),
+    }).describe("Campaign not found."),
+
+    500: z.object({
+      error: z.string(),
+    }).describe("Unexpected server error."),
+  },
+}
