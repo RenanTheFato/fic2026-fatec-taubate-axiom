05ea7ecc2992b2abec55a71b568ae615a50ed793
RenanTheFato <renan.santana007@hotmail.com>
2026-08-20 14:21:59 -0300

    add donor routes into index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { userRoutes } from "./user-routes.js";
 import { campaignRoutes } from "./campaign-routes.js";
+import { donorRoutes } from "./donor-routes.js";
 
 export const routes = Router()
 
@@ -12,3 +13,4 @@ routes.get("/ping", (req: Request, res: Response) => {
 
 routes.use("/user", userRoutes)
 routes.use("/campaign", campaignRoutes)
+routes.use("/donor", donorRoutes)
