d06a870ad044c399cbacf7a8ef501f0b2c87201b
RenanTheFato <renan.santana007@hotmail.com>
2026-08-20 16:28:20 -0300

    insert new routes into donor routes

--- backend/src/routes/donor-routes.ts
+++ backend/src/routes/donor-routes.ts
@@ -5,6 +5,8 @@ import { CreateDonorController } from "../controllers/donor/create-donor-control
 import { ListDonorsController } from "../controllers/donor/list-donors-controller.js";
 import { GetDonorController } from "../controllers/donor/get-donor-controller.js";
 import { GetDonorProfileController } from "../controllers/donor/get-donor-profile-controller.js";
+import { UpdateDonorController } from "../controllers/donor/update-donor-controller.js";
+import { AnonymizeDonorController } from "../controllers/donor/anonymize-donor-controller.js";
 
 export const donorRoutes = Router()
 
@@ -16,10 +18,19 @@ donorRoutes.get("/list", AuthMiddleware, RoleMiddleware("admin", "staff"), async
   return new ListDonorsController().handle(req, res)
 })
 
+// "/profile" precisa vir antes de "/:id", senão o express casa o caminho literal com o param
+donorRoutes.get("/profile", AuthMiddleware, async(req: Request, res: Response) => {
+  return new GetDonorProfileController().handle(req, res)
+})
+
 donorRoutes.get("/:id", AuthMiddleware, RoleMiddleware("admin", "staff"), async(req: Request, res: Response) => {
   return new GetDonorController().handle(req, res)
 })
 
-donorRoutes.get("/profile", AuthMiddleware, async(req: Request, res: Response) => {
-  return new GetDonorProfileController().handle(req, res)
+donorRoutes.put("/update/:id", AuthMiddleware, RoleMiddleware("admin", "staff"), async(req: Request, res: Response) => {
+  return new UpdateDonorController().handle(req, res)
+})
+
+donorRoutes.patch("/anonymize/:id", AuthMiddleware, RoleMiddleware("admin"), async(req: Request, res: Response) => {
+  return new AnonymizeDonorController().handle(req, res)
 })
