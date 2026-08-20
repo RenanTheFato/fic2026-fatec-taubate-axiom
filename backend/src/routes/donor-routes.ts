e73a767e12852e364335c123a77468eb46fb82e3
RenanTheFato <renan.santana007@hotmail.com>
2026-08-20 16:01:32 -0300

    add get donor profile into routes

--- backend/src/routes/donor-routes.ts
+++ backend/src/routes/donor-routes.ts
@@ -4,6 +4,7 @@ import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { CreateDonorController } from "../controllers/donor/create-donor-controller.js";
 import { ListDonorsController } from "../controllers/donor/list-donors-controller.js";
 import { GetDonorController } from "../controllers/donor/get-donor-controller.js";
+import { GetDonorProfileController } from "../controllers/donor/get-donor-profile-controller.js";
 
 export const donorRoutes = Router()
 
@@ -17,4 +18,8 @@ donorRoutes.get("/list", AuthMiddleware, RoleMiddleware("admin", "staff"), async
 
 donorRoutes.get("/:id", AuthMiddleware, RoleMiddleware("admin", "staff"), async(req: Request, res: Response) => {
   return new GetDonorController().handle(req, res)
+})
+
+donorRoutes.get("/profile", AuthMiddleware, async(req: Request, res: Response) => {
+  return new GetDonorProfileController().handle(req, res)
 })
