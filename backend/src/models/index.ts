b963c5eb98cf5b40d47dc142f14f43747e741852
RenanTheFato <renan.santana007@hotmail.com>
2026-08-20 14:19:59 -0300

    update model by adding foreign relations

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,5 +1,11 @@
 import { sequelize } from "../config/sequelize.js";
 import { User } from "./user-model.js";
 import { Campaign } from "./campaign-model.js";
+import { Donor } from "./donor-model.js";
 
-export { sequelize, User, Campaign }
+// Identity
+
+User.hasOne(Donor, { foreignKey: "user_id", as: "donor" })
+Donor.belongsTo(User, { foreignKey: "user_id", as: "user" })
+
+export { sequelize, User, Campaign, Donor }
