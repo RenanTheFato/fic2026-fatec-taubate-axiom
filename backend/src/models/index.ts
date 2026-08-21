7639e382cdf916cadf5a471a35a68831bfd42f9a
RenanTheFato <renan.santana007@hotmail.com>
2026-08-21 15:02:16 -0300

    add product model into sequelize index

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -2,10 +2,11 @@ import { sequelize } from "../config/sequelize.js";
 import { User } from "./user-model.js";
 import { Campaign } from "./campaign-model.js";
 import { Donor } from "./donor-model.js";
+import { Product } from "./product-model.js";
 
 // Identity
 
 User.hasOne(Donor, { foreignKey: "user_id", as: "donor" })
 Donor.belongsTo(User, { foreignKey: "user_id", as: "user" })
 
-export { sequelize, User, Campaign, Donor }
+export { sequelize, User, Campaign, Donor, Product }
