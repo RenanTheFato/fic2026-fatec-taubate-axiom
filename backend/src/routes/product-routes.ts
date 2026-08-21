148cf027d0fa49f88bd567d32a9121463ceaf020
RenanTheFato <renan.santana007@hotmail.com>
2026-08-21 15:33:06 -0300

    insert new list products into routes

--- backend/src/routes/product-routes.ts
+++ backend/src/routes/product-routes.ts
@@ -2,9 +2,14 @@ import { Router, Request, Response } from "express";
 import { AuthMiddleware } from "../middlewares/auth-middleware.js";
 import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { CreateProductController } from "../controllers/product/create-product-controller.js";
+import { ListProductsController } from "../controllers/product/list-products-controller.js";
 
 export const productRoutes = Router()
 
 productRoutes.post("/create", AuthMiddleware, RoleMiddleware("admin", "staff"), async (req: Request, res: Response) => {
   return new CreateProductController().handle(req, res)
+})
+
+productRoutes.get("/list", async(req: Request, res: Response) => {
+  return new ListProductsController().handle(req, res)
 })
