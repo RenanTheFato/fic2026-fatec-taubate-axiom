9edafb6fdcba17414788ec172a2e710af7a5304e
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 10:49:43 -0300

    add event and his foreign relations

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -3,10 +3,16 @@ import { User } from "./user-model.js";
 import { Campaign } from "./campaign-model.js";
 import { Donor } from "./donor-model.js";
 import { Product } from "./product-model.js";
+import { Event } from "./event-model.js";
 
 // Identity
 
 User.hasOne(Donor, { foreignKey: "user_id", as: "donor" })
 Donor.belongsTo(User, { foreignKey: "user_id", as: "user" })
 
-export { sequelize, User, Campaign, Donor, Product }
+// Campaign
+
+Campaign.hasMany(Event, { foreignKey: "campaign_id", as: "events" })
+Event.belongsTo(Campaign, { foreignKey: "campaign_id", as: "campaign" })
+
+export { sequelize, User, Campaign, Donor, Product, Event }
