9f82d0a0bb8ddf59b3ae0c8db7404d63fc15087e
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 11:34:35 -0300

    insert list events into routes

--- backend/src/routes/event-routes.ts
+++ backend/src/routes/event-routes.ts
@@ -2,9 +2,14 @@ import { Router, Request, Response } from "express";
 import { AuthMiddleware } from "../middlewares/auth-middleware.js";
 import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { CreateEventController } from "../controllers/event/create-event-controller.js";
+import { ListEventsController } from "../controllers/event/list-events-controller.js";
 
 export const eventRoutes = Router()
 
 eventRoutes.post("/create", AuthMiddleware, RoleMiddleware("admin", "staff"), async (req: Request, res: Response) => {
   return new CreateEventController().handle(req, res)
+})
+
+eventRoutes.get("/list", async (req: Request, res: Response) => {
+  return new ListEventsController().handle(req, res)
 })
