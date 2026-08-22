b9fb48cc1966a46ebb568a4f45458fef56d4e00e
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 09:43:15 -0300

    create logic to activate an product

---
+++ backend/src/controllers/product/activate-product-controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { ProductInterface } from "../../interfaces/product-interface.js";
+import { ActivateProductService } from "../../services/product/activate-product-service.js";
+import { BadRequestError, NotFoundError } from "../../config/errors.js";
+
+export class ActivateProductController {
+  async handle(req: Request, res: Response) {
+    const { id } = req.params as Pick<ProductInterface, 'id'>
+
+    if (!id) {
+      return res.status(400).json({ error: "The product id is required" })
+    }
+
+    try {
+      const activateProductService = new ActivateProductService()
+      await activateProductService.execute({ id })
+
+      return res.status(200).json({ message: "Product is now active" })
+    } catch (error: unknown) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ error: error.message })
+      }
+
+      if (error instanceof BadRequestError) {
+        return res.status(400).json({ error: error.message })
+      }
+
+      console.error(error)
+      return res.status(500).json({ error: "Internal Server Error" })
+    }
+  }
+}
---
+++ backend/src/services/product/activate-product-service.ts
@@ -0,0 +1,21 @@
+import { BadRequestError, NotFoundError } from "../../config/errors.js";
+import { ProductInterface } from "../../interfaces/product-interface.js";
+import { Product } from "../../models/product-model.js";
+
+export class ActivateProductService {
+  async execute({ id }: Pick<ProductInterface, 'id'>) {
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      throw new NotFoundError("Product not found")
+    }
+
+    if (product.active === true) {
+      throw new BadRequestError("The product is already active")
+    }
+
+    await product.update({
+      active: true
+    })
+  }
+}
