e350fa5fb8cc912262081eb9df0d5997ea2de820
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 08:42:17 -0300

    insert get product by id into routes

--- backend/src/routes/product-routes.ts
+++ backend/src/routes/product-routes.ts
@@ -4,6 +4,7 @@ import { RoleMiddleware } from "../middlewares/role-middleware.js";
 import { CreateProductController } from "../controllers/product/create-product-controller.js";
 import { ListProductsController } from "../controllers/product/list-products-controller.js";
 import { ListAllProductsController } from "../controllers/product/list-all-products-controller.js";
+import { GetProductByIdController } from "../controllers/product/get-product-by-id-controller.js";
 
 export const productRoutes = Router()
 
@@ -17,4 +18,8 @@ productRoutes.get("/list", async(req: Request, res: Response) => {
 
 productRoutes.get("/list-all", AuthMiddleware, RoleMiddleware("admin", "staff"), async(req: Request, res: Response) => {
   return new ListAllProductsController().handle(req, res)
+})
+
+productRoutes.get("/:id", async(req: Request, res: Response) => {
+  return new GetProductByIdController().handle(req, res)
 })
