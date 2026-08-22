a8ca7f795709bf85688532c889912137ea3b83da
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 09:55:52 -0300

    insert deactivate product into swagger

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -25,6 +25,7 @@ import { listAllProductsDoc } from "../docs/product/list-all-products.doc.js";
 import { getProductByIdDoc } from "../docs/product/get-product-by-id.doc.js";
 import { updateProductDoc } from "../docs/product/update-product.doc.js";
 import { activateProductDoc } from "../docs/product/activate-product.doc.js";
+import { deactivateProductDoc } from "../docs/product/deactivate-product.doc.js";
 
 function toOperation<T extends ApiDoc>(doc: T): ZodOpenApiOperationObject {
   return {
@@ -93,5 +94,6 @@ export const openApiDocument = createDocument({
     "/product/{id}": { get: toOperation(getProductByIdDoc) },
     "/product/update/{id}": { put: toOperation(updateProductDoc) },
     "/product/activate/{id}": { patch: toOperation(activateProductDoc) },
+    "/product/deactivate/{id}": { patch: toOperation(deactivateProductDoc) },
   },
 })
