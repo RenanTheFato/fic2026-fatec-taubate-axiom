a9d1d23b821fb08aae6c4e2af98ea72e64ee69a6
RenanTheFato <renan.santana007@hotmail.com>
2026-08-22 09:48:02 -0300

    create activate product doc

---
+++ backend/src/docs/product/activate-product.doc.ts
@@ -0,0 +1,42 @@
+import { z } from "zod/v4";
+
+export const activateProductDoc = {
+  tags: ["product"],
+  summary: "Activate an Product",
+  description: "Turn an deactivate product into an active and visible publicly",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  params: z.object({
+    id: z.uuid()
+      .describe("Identifier of the product.")
+      .meta({ example: "0b7f5a12-9c4e-4f8a-9d2b-6a1f3e5c7d90" }),
+  }),
+  response: {
+    200: z.object({
+      message: z.string()
+    }).describe("Activated sucessful"),
+
+    400: z.object({
+      error: z.string(),
+    }).describe("Bad Request — Validation failure or business rule violation."),
+
+    401: z.object({
+      error: z.string(),
+    }).describe("Unauthorized. Missing, invalid or expired JWT token."),
+
+    403: z.object({
+      error: z.string()
+    }).describe("Forbidden — The authenticated user's role is not allowed to activate products."),
+
+    404: z.object({
+      error: z.string(),
+    }).describe("Product not found."),
+
+    500: z.object({
+      error: z.string(),
+    }).describe("Unexpected server error."),
+  },
+}
