b71aa40e81bfddc6a5c382390600680960745ebc
RenanTheFato <renan.santana007@hotmail.com>
2026-09-01 20:04:51 -0300

    add test configuration into vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+/// <reference types="vitest/config" />
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/tests/setup.ts"],
+    include: ["src/tests/**/*.test.{ts,tsx}"],
+  },
 })
